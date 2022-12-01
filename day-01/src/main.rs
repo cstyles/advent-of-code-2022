265807ca4e017bcff80ec6c45401c1717b4927ea
Collin Styles <collingstyles@gmail.com>
2022-12-01 14:20:09 -0800

    Day 01, part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
     let input = include_str!("../input.txt");
 
-    let part1: u32 = input
+    let mut elves: Vec<u32> = input
         .split("\n\n")
         .map(|elf| elf.lines().map(|line| line.parse::<u32>().unwrap()).sum())
-        .max()
-        .unwrap();
+        .collect();
+
+    elves.sort();
+
+    let part1 = elves.last().unwrap();
+    let part2: u32 = elves.iter().rev().take(3).sum();
 
     println!("part1 = {part1}");
+    println!("part2 = {part2}");
 }
