67f8936cd8afb0d32f857acad87ba29179384a2f
Collin Styles <collingstyles@gmail.com>
2022-12-09 21:28:47 -0800

    Day 10, part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -23,6 +23,32 @@ impl From<&str> for Instruction {
     }
 }
 
+#[derive(Default, Debug, Clone, Copy)]
+struct Point {
+    row: usize,
+    column: usize,
+}
+
+impl Point {
+    fn advance(&mut self) {
+        self.column += 1;
+
+        // TODO: ugh
+        if self.column >= 40 {
+            self.column = 0;
+            self.row += 1;
+
+            if self.row >= 6 {
+                self.row = 0;
+            }
+        }
+    }
+
+    fn in_range(&self, x: i32) -> bool {
+        (x - 1..=x + 1).contains(&(self.column as i32))
+    }
+}
+
 fn main() {
     let input = match std::env::var("TEST") {
         Ok(number) => read_to_string(format!("test_input{number}.txt")),
@@ -35,12 +61,18 @@ fn main() {
     let mut x = 1;
     let mut signal_strengths = vec![];
 
+    let mut screen = [[false; 40]; 6];
+    let mut pixel_being_drawn = Point::default();
+
     for instruction in instructions {
         for _ in 0..instruction.cycles() {
             clock += 1;
             if (clock - 20) % 40 == 0 {
                 signal_strengths.push(x * clock);
             }
+
+            screen[pixel_being_drawn.row][pixel_being_drawn.column] = pixel_being_drawn.in_range(x);
+            pixel_being_drawn.advance();
         }
 
         if let Instruction::Add(v) = instruction {
@@ -50,4 +82,18 @@ fn main() {
 
     let part1: i32 = signal_strengths.into_iter().sum();
     println!("part1 = {part1}");
+
+    draw(screen);
+}
+
+fn draw(screen: [[bool; 40]; 6]) {
+    for row in screen {
+        for pixel in row {
+            match pixel {
+                true => print!("#"),
+                false => print!("."),
+            };
+        }
+        println!();
+    }
 }
