21293176c7c592c7a62a030b29faef355a663ac8
Collin Styles <collingstyles@gmail.com>
2022-12-18 12:15:17 -0500

    Day 18, part 1

---
+++ day-18/src/main.rs
@@ -0,0 +1,108 @@
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
+struct Point {
+    z: isize,
+    y: isize,
+    x: isize,
+}
+
+impl From<&str> for Point {
+    fn from(input: &str) -> Self {
+        let (z, rest) = input.split_once(',').unwrap();
+        let (y, x) = rest.split_once(',').unwrap();
+
+        let z = z.parse().unwrap();
+        let y = y.parse().unwrap();
+        let x = x.parse().unwrap();
+
+        Self { z, y, x }
+    }
+}
+
+impl Point {
+    fn neighbors(&self) -> [Self; 6] {
+        [
+            self.up(),
+            self.down(),
+            self.left(),
+            self.right(),
+            self.in_(),
+            self.out(),
+        ]
+    }
+
+    fn up(self) -> Self {
+        Self {
+            y: self.y + 1,
+            ..self
+        }
+    }
+
+    fn down(self) -> Self {
+        Self {
+            y: self.y - 1,
+            ..self
+        }
+    }
+
+    fn left(self) -> Self {
+        Self {
+            x: self.x - 1,
+            ..self
+        }
+    }
+
+    fn right(self) -> Self {
+        Self {
+            x: self.x + 1,
+            ..self
+        }
+    }
+
+    fn in_(self) -> Self {
+        Self {
+            z: self.z - 1,
+            ..self
+        }
+    }
+
+    fn out(self) -> Self {
+        Self {
+            z: self.z + 1,
+            ..self
+        }
+    }
+}
+
+fn main() {
+    let input = if std::env::var("TEST").is_ok() {
+        include_str!("../test_input.txt")
+    } else {
+        include_str!("../input.txt")
+    };
+
+    let points: Vec<_> = input.lines().map(Point::from).collect();
+
+    let mut grid = vec![vec![vec![false; 33]; 33]; 33];
+
+    let mut surface_area = 0;
+    for point in points {
+        *lookup_mut(&mut grid, point) = true;
+        surface_area += 6;
+
+        for neighbor in point.neighbors() {
+            if lookup(&grid, neighbor) {
+                surface_area -= 2;
+            }
+        }
+    }
+
+    println!("part1 = {surface_area}");
+}
+
+fn lookup(grid: &[Vec<Vec<bool>>], point: Point) -> bool {
+    grid[(point.z + 10) as usize][(point.y + 10) as usize][(point.x + 10) as usize]
+}
+
+fn lookup_mut(grid: &mut [Vec<Vec<bool>>], point: Point) -> &mut bool {
+    &mut grid[point.z as usize + 10][point.y as usize + 10][point.x as usize + 10]
+}
