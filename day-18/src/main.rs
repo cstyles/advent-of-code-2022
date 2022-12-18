11c161f7c859a6bd8e22b765d0e9b5c155dba3c3
Collin Styles <collingstyles@gmail.com>
2022-12-18 16:24:24 -0500

    Day 18, part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
 struct Point {
     z: isize,
@@ -71,6 +73,10 @@ impl Point {
             ..self
         }
     }
+
+    fn escaped(&self) -> bool {
+        self.z > 33 || self.z < -10 || self.y > 33 || self.y < -10 || self.x > 33 || self.x < -10
+    }
 }
 
 fn main() {
@@ -80,23 +86,59 @@ fn main() {
         include_str!("../input.txt")
     };
 
-    let points: Vec<_> = input.lines().map(Point::from).collect();
+    let points: HashSet<Point> = input.lines().map(Point::from).collect();
 
     let mut grid = vec![vec![vec![false; 33]; 33]; 33];
 
+    part1(&mut grid, &points);
+    part2(&points);
+}
+
+fn part1(grid: &mut [Vec<Vec<bool>>], points: &HashSet<Point>) {
     let mut surface_area = 0;
     for point in points {
-        *lookup_mut(&mut grid, point) = true;
+        *lookup_mut(grid, *point) = true;
         surface_area += 6;
 
         for neighbor in point.neighbors() {
-            if lookup(&grid, neighbor) {
+            if lookup(grid, neighbor) {
                 surface_area -= 2;
             }
         }
     }
 
     println!("part1 = {surface_area}");
+}
+
+fn part2(points: &HashSet<Point>) {
+    let mut queue = VecDeque::from([Point {
+        z: 22,
+        y: 22,
+        x: 22,
+    }]);
+    let mut outside: HashSet<Point> = HashSet::from_iter(queue.iter().copied());
+
+    while let Some(point) = queue.pop_front() {
+        for neighbor in point.neighbors() {
+            if outside.contains(&neighbor) || neighbor.escaped() || points.contains(&neighbor) {
+                continue;
+            }
+
+            outside.insert(neighbor);
+            queue.push_back(neighbor);
+        }
+    }
+
+    let mut surface_area = 0;
+    for point in points {
+        for neighbor in point.neighbors() {
+            if outside.contains(&neighbor) {
+                surface_area += 1;
+            }
+        }
+    }
+
+    println!("part2 = {surface_area}")
 }
 
 fn lookup(grid: &[Vec<Vec<bool>>], point: Point) -> bool {
