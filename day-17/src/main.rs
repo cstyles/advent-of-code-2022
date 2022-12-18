20354e65534db128b43c5f62498954d4bd541ed0
Collin Styles <collingstyles@gmail.com>
2022-12-17 19:29:27 -0500

    Day 17, part 2
    holy shit this sucks

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -290,13 +290,21 @@ impl Tower {
         }
     }
 
-    fn lock_in_piece(&mut self, piece: Piece) {
+    fn lock_in_piece(&mut self, piece: Piece) -> Option<usize> {
         self.draw_piece(piece);
 
         // TODO: inefficient
         for _ in 0..piece.kind.height() {
             self.grid.push(Default::default());
         }
+
+        for row in piece.position.y - 2..=piece.position.y + 2 {
+            if self.grid[row].into_iter().all(|tile| tile == Rock) {
+                return Some(row);
+            }
+        }
+
+        None
     }
 
     #[allow(unused)]
@@ -327,6 +335,10 @@ impl Tower {
 
         false
     }
+
+    pub fn top_is_floor(&self) -> bool {
+        self.grid[self.top].into_iter().all(|tile| tile == Rock)
+    }
 }
 
 fn main() {
@@ -340,37 +352,28 @@ fn main() {
     let mut jets = input.trim_end().chars().map(Direction::from).cycle();
     let mut piece_kinds = [Line, Plus, Ell, Column, Square].into_iter().cycle();
     let mut rocks_fallen = 0;
-    // println!("Start:");
-    // tower.print();
 
-    while rocks_fallen < 2022 {
-        // std::thread::sleep(std::time::Duration::from_millis(50));
+    #[derive(Debug)]
+    enum MyOption {
+        None1,
+        None2,
+        Some(usize),
+    }
 
+    let mut rocks_before_loop = MyOption::None1;
+    let mut height_before_loop = 0;
+    let mut cycles = 0;
+    let mut rocks_per_loop = None;
+    let mut height_per_loop = 0;
+
+    while rocks_fallen < 5_022 {
         let piece_kind = piece_kinds.next().unwrap();
         let mut piece = Piece::new(piece_kind, tower.top);
-        // println!("New piece:");
 
         // Until piece rests
         loop {
-            // tower.draw_piece(piece);
-            // tower.print();
-            // tower.erase_piece(piece);
-
             let jet_direction = jets.next().unwrap();
-            // println!("trying to move {jet_direction:?}");
-            let shifted = piece.shift(jet_direction, &tower);
-
-            // if shifted.is_some() {
-            //     println!("moved {jet_direction:?}");
-            // } else {
-            //     println!("didn't move horizontally");
-            // }
-
-            // tower.draw_piece(piece);
-            // tower.print();
-            // tower.erase_piece(piece);
-
-            // println!("Descending");
+            let _shifted = piece.shift(jet_direction, &tower);
             let descended = piece.descend(&tower);
 
             if descended.is_none() {
@@ -378,13 +381,99 @@ fn main() {
             }
         }
 
-        // dbg!(piece);
-        tower.lock_in_piece(piece);
+        let completed_row = tower.lock_in_piece(piece);
         tower.recalculate_top();
         rocks_fallen += 1;
-        // tower.print();
+
+        if let Some(_completed_row) = completed_row {
+            // dbg!(completed_row);
+            // dbg!(rocks_fallen);
+            // dbg!(tower.top);
+            // println!();
+
+            match rocks_before_loop {
+                MyOption::None1 => rocks_before_loop = MyOption::None2,
+                MyOption::None2 => {
+                    rocks_before_loop = MyOption::Some(rocks_fallen);
+                    height_before_loop = tower.top;
+                }
+                MyOption::Some(rocks_before_loop) => {
+                    cycles += 1;
+                    if cycles == 18 {
+                        rocks_per_loop = Some(rocks_fallen - rocks_before_loop);
+                        height_per_loop = tower.top - height_before_loop;
+                        break;
+                    }
+                }
+            };
+
+            // tower.print();
+        }
     }
 
-    // tower.print();
     println!("part1 = {}", tower.top);
+    // tower.print();
+
+    // for (i, row) in tower.grid.into_iter().enumerate() {
+    //     if row.into_iter().all(|tile| tile == Rock) {
+    //         dbg!(i);
+    //     }
+    // }
+
+    let MyOption::Some(rocks_before_loop) = rocks_before_loop else {
+        panic!();
+    };
+    let height_before_loop = height_before_loop;
+    let rocks_per_loop = rocks_per_loop.unwrap();
+
+    dbg!(height_before_loop);
+    dbg!(rocks_before_loop);
+    println!();
+    dbg!(rocks_per_loop);
+    dbg!(height_per_loop);
+
+    let remaining_rocks = 1_000_000_000_000 - rocks_before_loop;
+    let loops = remaining_rocks / rocks_per_loop;
+    let height = height_before_loop + height_per_loop * loops;
+
+    let mut remaining_rocks = remaining_rocks - loops * rocks_per_loop;
+
+    println!();
+    dbg!(remaining_rocks);
+    dbg!(loops);
+    dbg!(height);
+    let tower_top_before_final_loops = tower.top;
+
+    while remaining_rocks != 0 {
+        (piece_kinds, jets) = drop_rock(&mut tower, piece_kinds, jets);
+        remaining_rocks -= 1;
+    }
+
+    let height = height + tower.top - tower_top_before_final_loops;
+    dbg!(height);
+}
+
+fn drop_rock<I: Iterator<Item = PieceKind>, J: Iterator<Item = Direction>>(
+    tower: &mut Tower,
+    mut piece_kinds: I,
+    mut jets: J,
+) -> (I, J) {
+    let piece_kind = piece_kinds.next().unwrap();
+    let mut piece = Piece::new(piece_kind, tower.top);
+
+    // Until piece rests
+    loop {
+        let jet_direction = jets.next().unwrap();
+        let _shifted = piece.shift(jet_direction, tower);
+        let descended = piece.descend(tower);
+
+        if descended.is_none() {
+            break;
+        }
+    }
+
+    let _completed_row = tower.lock_in_piece(piece);
+    tower.recalculate_top();
+
+    (piece_kinds, jets)
 }
