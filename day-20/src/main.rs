9c74dc3045d847d0834d062add19e0cad696e48e
Collin Styles <collingstyles@gmail.com>
2022-12-20 17:55:53 -0800

    Day 20, part 1

---
+++ day-20/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    let input = if std::env::var("TEST").is_ok() {
+        include_str!("../test_input.txt")
+    } else {
+        include_str!("../input.txt")
+    };
+
+    let mut numbers: Vec<(usize, i64)> = input
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .enumerate()
+        .collect();
+
+    let length = numbers.len();
+    for index in 0..length {
+        let index_of_number_to_move = numbers.iter().position(|(i, _)| *i == index).unwrap();
+        let original_number_to_move = numbers[index_of_number_to_move].1;
+        let mut how_much_to_move = original_number_to_move % (length - 1) as i64;
+
+        if how_much_to_move.is_positive() {
+            let wrap_off_right = index_of_number_to_move as i64 + how_much_to_move >= length as i64;
+            if wrap_off_right {
+                // turn into negative
+                how_much_to_move = how_much_to_move - length as i64 + 1;
+            }
+        } else if how_much_to_move.is_negative() {
+            let wrap_off_left = index_of_number_to_move as i64 + how_much_to_move <= 0;
+            if wrap_off_left {
+                // turn into positive
+                how_much_to_move = length as i64 + how_much_to_move - 1;
+            }
+        } else {
+            continue;
+        }
+
+        if how_much_to_move.is_positive() {
+            let target = index_of_number_to_move + how_much_to_move as usize;
+            let start = index_of_number_to_move + 1;
+            let range = start..=target;
+            numbers.copy_within(range, index_of_number_to_move);
+            numbers[target] = (index, original_number_to_move);
+        } else {
+            let target = (index_of_number_to_move as i64 + how_much_to_move) as usize;
+            let range = target..index_of_number_to_move;
+            numbers.copy_within(range, target + 1);
+            numbers[target] = (index, original_number_to_move);
+        }
+    }
+
+    let zero_index = numbers
+        .iter()
+        .position(|(_index, number)| *number == 0)
+        .unwrap();
+
+    let part1: i64 = numbers
+        .iter()
+        .map(|(_, number)| number)
+        .cycle()
+        .skip(zero_index)
+        .step_by(1_000)
+        .skip(1)
+        .take(3)
+        .sum();
+
+    println!("part1 = {part1:?}");
+}
