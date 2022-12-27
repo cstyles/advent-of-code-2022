6cfd5bdb2d0f66f92deb2cff027e73dedbd4a89b
Collin Styles <collingstyles@gmail.com>
2022-12-27 11:52:32 -0800

    Day 25: Implement addition on SNAFU numbers directly
    Without converting to & from decimal.

--- day-25/src/main.rs
+++ day-25/src/main.rs
@@ -1,3 +1,6 @@
+use std::iter::Sum;
+use std::ops::Add;
+
 fn parse_snafu_digit(c: char) -> i64 {
     match c {
         '2' => 2,
@@ -52,6 +55,132 @@ fn as_snafu(mut num: i64) -> String {
     string.chars().rev().collect()
 }
 
+use SnafuDigit::*;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum SnafuDigit {
+    Zero,
+    One,
+    Two,
+    NegOne,
+    NegTwo,
+}
+
+impl Add<SnafuDigit> for SnafuDigit {
+    // Returns the digit and a second digit indicating carry
+    type Output = (SnafuDigit, SnafuDigit);
+
+    fn add(self, rhs: SnafuDigit) -> Self::Output {
+        match (self, rhs) {
+            (Zero, _) => (rhs, Zero),
+            (_, Zero) => (self, Zero),
+            (One, One) => (Two, Zero),
+            (One, Two) => (NegTwo, One),
+            (One, NegOne) => (Zero, Zero),
+            (One, NegTwo) => (NegOne, Zero),
+            (Two, One) => (NegTwo, One),
+            (Two, Two) => (NegOne, One),
+            (Two, NegOne) => (One, Zero),
+            (Two, NegTwo) => (Zero, Zero),
+            (NegOne, One) => (Zero, Zero),
+            (NegOne, Two) => (One, Zero),
+            (NegOne, NegOne) => (NegTwo, Zero),
+            (NegOne, NegTwo) => (Two, NegOne),
+            (NegTwo, One) => (NegOne, Zero),
+            (NegTwo, Two) => (Zero, Zero),
+            (NegTwo, NegOne) => (Two, NegOne),
+            (NegTwo, NegTwo) => (One, NegOne),
+        }
+    }
+}
+
+impl From<char> for SnafuDigit {
+    fn from(c: char) -> Self {
+        match c {
+            '2' => Two,
+            '1' => One,
+            '0' => Zero,
+            '-' => NegOne,
+            '=' => NegTwo,
+            _ => panic!("bad char: {c}"),
+        }
+    }
+}
+
+impl SnafuDigit {
+    fn as_char(&self) -> char {
+        match *self {
+            NegTwo => '=',
+            NegOne => '-',
+            Zero => '0',
+            One => '1',
+            Two => '2',
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+struct Snafu {
+    digits: Vec<SnafuDigit>,
+}
+
+impl<T: AsRef<str>> From<T> for Snafu {
+    fn from(value: T) -> Self {
+        let value = value.as_ref();
+        let digits = value.chars().rev().map(SnafuDigit::from).collect();
+
+        Self { digits }
+    }
+}
+
+impl std::fmt::Display for Snafu {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s: String = self.digits.iter().rev().map(SnafuDigit::as_char).collect();
+        write!(f, "{s}")
+    }
+}
+
+impl Add<Snafu> for Snafu {
+    type Output = Snafu;
+
+    fn add(self, rhs: Snafu) -> Self::Output {
+        let mut digits = Vec::with_capacity(self.digits.len().max(rhs.digits.len()));
+        let mut i = 0;
+        let mut previous_carry = Zero;
+
+        loop {
+            let (self_digit, rhs_digit) = match (self.digits.get(i), rhs.digits.get(i)) {
+                (None, None) => break,
+                (None, Some(rhs_digit)) => (Zero, *rhs_digit),
+                (Some(self_digit), None) => (*self_digit, Zero),
+                (Some(self_digit), Some(rhs_digit)) => (*self_digit, *rhs_digit),
+            };
+
+            let (digit, new_carry) = self_digit + rhs_digit;
+            let (digit, carry_carry) = digit + previous_carry;
+            let (next_carry, carry_that_is_always_zero) = new_carry + carry_carry;
+            assert_eq!(Zero, carry_that_is_always_zero);
+
+            digits.push(digit);
+            previous_carry = next_carry;
+
+            i += 1;
+        }
+
+        if previous_carry != Zero {
+            digits.push(previous_carry);
+        }
+
+        Snafu { digits }
+    }
+}
+
+impl Sum for Snafu {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Snafu::default(), |acc, x| acc + x)
+    }
+}
+
 fn main() {
     let input = if std::env::var("TEST").is_ok() {
         include_str!("../test_input.txt")
@@ -60,6 +189,10 @@ fn main() {
     };
 
     let sum = input.lines().map(parse_snafu_number).sum();
-    let part1 = as_snafu(sum);
-    println!("part1 = {part1}");
+    let to_and_from_decimal = as_snafu(sum);
+
+    let direct: Snafu = input.lines().map(Snafu::from).sum();
+    assert_eq!(to_and_from_decimal, direct.to_string());
+
+    println!("part1 = {direct}");
 }
